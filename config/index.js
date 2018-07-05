262745c05de9623c08032b8df766aaccbb2a5a20
beth <zhangbaige@innobuddy.com>
2018-07-05 14:24:33 +0800

    修改初始化配置文件

--- config/index.js
+++ config/index.js
@@ -2,5 +2,15 @@ const serviceName = 'qiuge.me'; // 服务的名字，必选不能省
 
 module.exports = {
   serviceName: serviceName,
-  port: 8888
+  port: 8888,
+  "db": {
+    "database": "",
+    "user": "",
+    "password": "",
+    "options": {
+      "host": "",
+      "port": "",
+      "dialect": "postgres"
+    }
+  },
 }
