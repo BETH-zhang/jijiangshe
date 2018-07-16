1a2bc09994f6a11662317c51eff3f4f709d2c567
beth <zhangbaige@innobuddy.com>
2018-07-16 15:16:16 +0800

    修改scripts初始化脚本

--- .roadhogrc.mock.js
+++ .roadhogrc.mock.js
@@ -9,6 +9,7 @@ import { format, delay } from 'roadhog-api-doc';
 
 // 是否禁用代理
 const noProxy = process.env.NO_PROXY === 'true';
+console.log('noProxy', noProxy, process.env.NO_PROXY);
 
 // 代码中会兼容本地 service mock 以及部署站点的静态数据
 const proxy = {
